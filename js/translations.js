b2731ff3c4ab3963eafbffb55fc7e17f797fcd93
Mohamedkassab2021 <forjobsa44@gmail.com>
2024-08-30 22:12:47 +0300

    test

--- js/translations.js
+++ js/translations.js
@@ -1,39 +1,39 @@
 const translations = {
-    en: {
-      siteName:"Muhammad kassap",
-      siteSubName:"Photography",
-      home: "Home",
-      information: "Pricing and packages",
-      portfolio: "Portfolio",
-      selectLanguage: "Select a language",
-      english: "English",
-      arabic: "Arabic",
-      pic:"Wedding Alliance Photography",
-      paragraph:"We try to make your day happy with our photography team",
-      welcome:"Welcome",
-      information:"Mohamed Kassab Photography",
-      info1:"Experience of not less than 6 years in the field of wedding and event photography",
-      info2:"And speed in delivery and maintain high image quality with the best print quality for albums and tablets and a lot of exclusive gifts at reasonable prices for all segments of society We wish you a nice 😍 day",
+  en: {
+    siteName: "Muhammad kassap",
+    siteSubName: "Photography",
+    home: "Home",
+    information: "Pricing and packages",
+    portfolio: "Portfolio",
+    selectLanguage: "Select a language",
+    english: "English",
+    arabic: "Arabic",
+    pic: "Wedding Alliance Photography",
+    paragraph: "We try to make your day happy with our photography team",
+    welcome: "Welcome",
+    information: "Mohamed Kassab Photography",
+    info1:
+      "Experience of not less than 6 years in the field of wedding and event photography",
+    info2:
+      "And speed in delivery and maintain high image quality with the best print quality for albums and tablets and a lot of exclusive gifts at reasonable prices for all segments of society We wish you a nice 😍 day",
+  },
+  ar: {
+    siteName: "محمد كساب",
+    siteSubName: "فوتوغرافى",
+    home: "الصفحة الرئيسية",
+    information: "التسعير و الباكدجات",
+    portfolio: "اعمالنا",
+    selectLanguage: "إختر لغة",
+    english: "الانجليزية",
+    arabic: "العربية",
+    pic: "تصوير حفل الزفاف",
+    paragraph: "نحن نحاول أن نجعل يومك سعيدًا مع فريق التصوير لدينا",
+    welcome: "اهلا بك",
+    information: "محمد كساب فوتوغرافى",
+    info1: "خبرة لا تقل عن 6 سنين فى مجال تصوير الافراح والمناسبات",
+    info2:
+      "وسرعة فى التسليم و الحفاظ على جودة الصور عالية مع افضل جودة طباعة للالبومات و التابلوهات و الكثير من الهدايا الحصرية باسعار مناسبة لجميع فئات المجتمع نتمنى لكم يوما لطيفا 😍",
+  },
+};
 
-
-    },
-    ar: {
-      siteName:"محمد كساب",
-      siteSubName:"فوتوغرافى",
-      home: "الصفحة الرئيسية",
-      information: "التسعير و الباكدجات",
-      portfolio: "اعمالنا",
-      selectLanguage: "إختر لغة",
-      english: "الانجليزية",
-      arabic: "العربية",
-      pic:"تصوير حفل الزفاف",
-      paragraph:"نحن نحاول أن نجعل يومك سعيدًا مع فريق التصوير لدينا",
-      welcome:"اهلا بك",
-      information:"محمد كساب فوتوغرافى",
-      info1:"خبرة لا تقل عن 6 سنين فى مجال تصوير الافراح والمناسبات",
-      info2:"وسرعة فى التسليم و الحفاظ على جودة الصور عالية مع افضل جودة طباعة للالبومات و التابلوهات و الكثير من الهدايا الحصرية باسعار مناسبة لجميع فئات المجتمع نتمنى لكم يوما لطيفا 😍",
-
-    },
-  };
-  
-  export default translations;
+export default translations;
