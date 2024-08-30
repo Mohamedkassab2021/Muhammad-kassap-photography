75d8547f9d2380130b6923d6e800e36d8a12c936
Mohamedkassab2021 <forjobsa44@gmail.com>
2024-08-30 22:35:01 +0300

    test

--- js/translations.js
+++ js/translations.js
@@ -10,7 +10,12 @@ const translations = {
     arabic: "Arabic",
     pic: "Wedding Alliance Photography",
     paragraph: "We try to make your day happy with our photography team",
-    welcome: "Welcome"
+    hy: "Welcome",
+    info:"Mohamed Kassab Photography",
+    info1:"Experience of not less than 6 years in the field of wedding and event photography",
+    info2:"And speed in delivery and maintain high image quality with the best print quality for albums and tablets and a lot of exclusive gifts at reasonable prices for all segments of society We wish you a nice 😍 day",
+    price:"Pricing and packages",
+    portfolio:"Portfolio"
   },
   ar: {
     siteName: "محمد كساب",
@@ -23,7 +28,12 @@ const translations = {
     arabic: "العربية",
     pic: "تصوير حفل الزفاف",
     paragraph: "نحن نحاول أن نجعل يومك سعيدًا مع فريق التصوير لدينا",
-    welcome: "اهلا بك"
+    hy: "اهلا بك",
+    info:"محمد كساب فوتوغرافى",
+    info1:"خبرة لا تقل عن 6 سنين فى مجال تصوير الافراح والمناسبات",
+    info2:"وسرعة فى التسليم و الحفاظ على جودة الصور عالية مع افضل جودة طباعة للالبومات و التابلوهات و الكثير من الهدايا الحصرية باسعار مناسبة لجميع فئات المجتمع نتمنى لكم يوما لطيفا 😍",
+    price:"الأسعار والحزم",
+    portfolio:"الصور والخدمات"
   },
 };
 
