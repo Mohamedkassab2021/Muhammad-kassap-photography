bcdac1dc25e201a205ba02ce6156e978f9cb1795
Mohamedkassab2021 <forjobsa44@gmail.com>
2024-09-15 13:22:14 +0300

    test

--- js/port.js
+++ js/port.js
@@ -1,15 +1,15 @@
-const progressCircle = document.querySelector(".autoplay-progress svg");
-const progressContent = document.querySelector(".autoplay-progress span");
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  effect: "fade",
-  centeredSlides: true,
-  autoplay: {
-    delay: 2000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+// const progressCircle = document.querySelector(".autoplay-progress svg");
+// const progressContent = document.querySelector(".autoplay-progress span");
+// var swiper = new Swiper(".mySwiper", {
+//   spaceBetween: 30,
+//   effect: "fade",
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 2000,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
